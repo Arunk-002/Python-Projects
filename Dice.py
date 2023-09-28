59560803487477717016eec69b090354f15b4462
arun <arunkumarjack7892gmail.com>
2023-09-28 13:01:45 +0530

    comments added for better readablity

--- Dice.py
+++ Dice.py
@@ -3,18 +3,19 @@ import random as rand
 
 class DiceRoller:
     def roller(self):
-        num = rand.randint(1, 6)
-        print("\n",num)
+        num = rand.randint(1, 6)  # Random number is genrated and assigned to num variable
+        print("\n", num)
 
 
-d = DiceRoller()
-e_val=input("Dice Roller"
-      "\nTo Roll your Dice:"
-      "\tPress 'y' to roll 'n' to cancel")
-if e_val=='y':
+d = DiceRoller()  # Instance created for the class
+
+e_val = input("Dice Roller"
+              "\nTo Roll your Dice:"
+              "\tPress 'y' to roll 'n' to cancel")  # Taking input from the user
+# Checking the input and providing appropriate response 
+if e_val == 'y':
     d.roller()
-elif e_val=='n':
+elif e_val == 'n':
     print("Roller Canceled")
 else:
     print("Plese select a valid option")
-
