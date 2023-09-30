3453373e265f8fa2c6812d8eb1b72ab11401152a
arun <arunkumarjack7892gmail.com>
2023-09-30 12:41:13 +0530

    image display issue fixed need to display image according to the dice number

--- Dice_gui.py
+++ Dice_gui.py
@@ -1,15 +1,28 @@
 from tkinter import *
+import random as rand
+from PIL import ImageTk, Image
+num = ""
+photo=''
+
+def roller():
+    global num,photo
+    # num = rand.randint(1, 6)
+    # msg.config(text=num)
+    photo = PhotoImage(file="1.png")
+    lbl.config(image=photo)
 
 
-window=Tk()
+window = Tk()
 window.title("Dice Roller 🎲")
-window.geometry("270x200")
+window.geometry("700x700")
 window.configure(bg="#9fc8fc")
 
 
+btn = Button(window, text="Roll", command=roller)
+btn.place(relx=0.5, rely=0.9, anchor=CENTER)
 
-
-
+lbl=Label(image=photo)
+lbl.place(relx=.1,rely=0)
 
 
 window.mainloop()
