3737b7872445a71a28a0f6634695ad6e25e4f2e5
arun <arunkumarjack7892gmail.com>
2023-10-02 12:41:28 +0530

    dice image project cmpleted. now need to add animation to before loading the image.

--- Dice_gui.py
+++ Dice_gui.py
@@ -1,14 +1,19 @@
 from tkinter import *
 import random as rand
-from PIL import ImageTk, Image
+
+
 num = ""
-photo=''
+photo = ''
+photo_dict = {1: "1.png", 2: "2.png", 3: "3.png", 4: "4.png", 5: "5.png", 6: "6.png"}
+
 
 def roller():
-    global num,photo
-    # num = rand.randint(1, 6)
-    # msg.config(text=num)
-    photo = PhotoImage(file="1.png")
+    global num, photo, photo_dict
+    num = rand.randint(1, 6)
+    for i in photo_dict:
+        if i == num:
+            p_file = photo_dict[i]
+    photo = PhotoImage(file=p_file)
     lbl.config(image=photo)
 
 
@@ -17,12 +22,10 @@ window.title("Dice Roller 🎲")
 window.geometry("700x700")
 window.configure(bg="#9fc8fc")
 
-
-btn = Button(window, text="Roll", command=roller)
+btn = Button(window, text="Roll", command=roller, width=15, height=3)
 btn.place(relx=0.5, rely=0.9, anchor=CENTER)
 
-lbl=Label(image=photo)
-lbl.place(relx=.1,rely=0)
-
+lbl = Label(image=photo)
+lbl.place(relx=.1, rely=.1)
 
 window.mainloop()
