77ceaf67a2b47c13dec497875296222f74413d76
arun <arunkumarjack7892gmail.com>
2023-10-03 12:35:23 +0530

    dice with animation has finished.need to add comments

--- Dice_gui.py
+++ Dice_gui.py
@@ -1,13 +1,14 @@
 from tkinter import *
 import random as rand
-
+from PIL import Image, ImageTk, ImageSequence
 
 num = ""
-photo = ''
+photo = ""
+img = None
 photo_dict = {1: "1.png", 2: "2.png", 3: "3.png", 4: "4.png", 5: "5.png", 6: "6.png"}
 
 
-def roller():
+def roller_img():
     global num, photo, photo_dict
     num = rand.randint(1, 6)
     for i in photo_dict:
@@ -17,15 +18,24 @@ def roller():
     lbl.config(image=photo)
 
 
+def roll_animation():
+    global img
+    img = Image.open("dice.gif")
+    for img in ImageSequence.Iterator(img):
+        img = ImageTk.PhotoImage(img)
+        lbl.config(image=img)
+        window.update()
+    roller_img()
+
+
 window = Tk()
 window.title("Dice Roller 🎲")
 window.geometry("700x700")
 window.configure(bg="#9fc8fc")
 
-btn = Button(window, text="Roll", command=roller, width=15, height=3)
-btn.place(relx=0.5, rely=0.9, anchor=CENTER)
-
-lbl = Label(image=photo)
-lbl.place(relx=.1, rely=.1)
+lbl = Label(window, image=photo)
+lbl.pack()
+btn = Button(window, text="Roll", command=roll_animation, width=15, height=3)
+btn.pack()
 
 window.mainloop()
