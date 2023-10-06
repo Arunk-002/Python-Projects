37dfa20a616d304d64fdac74795d540eea4efdf5
arun <arunkumarjack7892gmail.com>
2023-10-06 11:39:10 +0530

    cv project starting .

--- Django_cv/CV/base/urls.py
+++ Django_cv/CV/base/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
-    path('',views.index ,name="index")
+    path('',views.Home ,name="home")
 ]
--- Django_cv/CV/base/views.py
+++ Django_cv/CV/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse(" my cv")
+def Home(request):
+    return render(request,'base/index.html')
