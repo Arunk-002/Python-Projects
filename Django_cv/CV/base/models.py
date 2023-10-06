da77f6b098860b0786df527eb59ab4172cdf6e23
arun <arunkumarjack7892gmail.com>
2023-10-06 12:37:15 +0530

    trying to get values from skills table.but not getting

--- Django_cv/CV/base/models.py
+++ Django_cv/CV/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class skills(models.Model):
+    skill=models.CharField(max_length=100)
+    skill_pic=models.ImageField(blank=False)
+
+    def __str__(self):
+        return self.skill
--- Django_cv/CV/base/views.py
+++ Django_cv/CV/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from base.models import skills
 
 # Create your views here.
 def Home(request):
-    return render(request,'base/index.html')
+    skils=skills.objects.all()
+    context={skils:'skils'}
+    return render(request,'base/index.html',context)
